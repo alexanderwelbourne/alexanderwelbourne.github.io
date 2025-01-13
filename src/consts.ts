1c166e21dc1f8844bb9be0c76a714d8f349ddb74
Alexander Welbourne <34348787+alexanderwelbourne@users.noreply.github.com>
2025-01-13 00:42:38 +0000

    Start changing the branding to me!
    - site name
    - brand.svg
    - Update some SEO info
    - Change the upfront landing page text

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "welbourne.ai",
+  DESCRIPTION: "Welcome to welbourne.ai, a portfolio and blog for designers and developers.",
+  AUTHOR: "Alexander Welbourne",
 }
 
 // Work Page
