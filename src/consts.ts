96d6ff24ab8dda6e7c4009572014335a6339eb15
Alexander Welbourne <34348787+alexanderwelbourne@users.noreply.github.com>
2025-01-18 21:47:01 +0000

    Update socials

--- src/consts.ts
+++ src/consts.ts
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "dev@welbourne.ai",
+    HREF: "mailto:dev@welbourne.ai",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "alexanderwelbourne",
+    HREF: "https://github.com/alexanderwelbourne"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "Alex Welbourne",
+    HREF: "https://www.linkedin.com/in/alex-welbourne-598ba5191/",
   },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+  // { 
+  //   NAME: "Twitter",
+  //   ICON: "twitter-x",
+  //   TEXT: "markhorn_dev",
+  //   HREF: "https://twitter.com/markhorn_dev",
+  // },
 ]
 
